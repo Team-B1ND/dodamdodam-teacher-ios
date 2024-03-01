cc0356190a0ddd93b3fef7ee967aba2c2a6576d3
Mercen <mercen@mercen.net>
2024-03-01 09:56:01 +0900

    Initial Commit

---
+++ DodamDodam-Teacher/DodamDodam_TeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodamDodam_TeacherApp.swift
+//  DodamDodam-Teacher
+//
+//  Created by Mercen on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DodamDodam_TeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
